944046b64f65f4c3503668ec8e984621b6b821e6
nikolausm <michael.nikolaus@gmail.com>
2020-03-03 00:02:44 +0100

    Update NewCustomerRequest.cs

--- WebApi/Models/NewCustomerRequest.cs
+++ WebApi/Models/NewCustomerRequest.cs
@@ -5,10 +5,17 @@ namespace WebApi.Models
 {
     public class NewCustomerRequest
     {
+        [JsonConstructor]
+        public NewCustomerRequest(string firstName, string fastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        
         [Required]
-        public string FirstName { get; set; }
+        public string FirstName { get; }
 
         [Required]
-        public string LastName { get; set; }
+        public string LastName { get; }
     }
 }
